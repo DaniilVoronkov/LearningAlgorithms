e03246279d9897f5312649e458deb164c821a089
DaniilVoronkov <79176087+DaniilVoronkov@users.noreply.github.com>
2023-03-29 20:18:12 -0400

    Added another method to the ChangedClass

--- src/ChangedClass.java
+++ src/ChangedClass.java
@@ -1,9 +1,14 @@
 public class ChangedClass {
     public static void main(String[] args) {
         methodName();
+        System.out.println(mainTest());
     }
 
     public static void methodName() {
 
+    }
+
+    public static int mainTest() {
+        return 0;
     }
 }
